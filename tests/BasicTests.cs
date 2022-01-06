87fc387f5436acc945b70b439185db0778193f16
messnerd <messnera@seattleu.edu>
2022-01-05 20:40:12 -0800

    commenting out sqrt test requirements

--- tests/BasicTests.cs
+++ tests/BasicTests.cs
@@ -29,7 +29,7 @@ namespace CalcC.Tests
         [DataRow("3 4 *", "12", "multiplication")]
         [DataRow("4 15 /", "3", "division")]
         [DataRow("5 7 %", "2", "modulus")]
-        [DataRow("16 sqrt", "4", "sqrt")]
+        // [DataRow("16 sqrt", "4", "sqrt")]
         [DataRow("-1 -1 *", "1", "negative operands")]
         public void TestManySimpleExpressions(string expr, string expectedResult, string label)
         {
@@ -48,26 +48,26 @@ namespace CalcC.Tests
             }
         }
 
-        [DataTestMethod]
-        [DataRow("-16 sqrt", typeof(OverflowException), "sqrt of negative number")]
-        public void TestForExceptions(string expr, Type expectedExceptionType, string label)
-        {
-            lock (monitor)
-            {
-                // Arrange handled by DataRows
+        // [DataTestMethod]
+        // [DataRow("-16 sqrt", typeof(OverflowException), "sqrt of negative number")]
+        // public void TestForExceptions(string expr, Type expectedExceptionType, string label)
+        // {
+        //     lock (monitor)
+        //     {
+        //         // Arrange handled by DataRows
 
-                // Act and Assert
-                var exception = Assert.ThrowsException<System.Reflection.TargetInvocationException>(() =>
-                {
-                    var calcc = new CalcC();
-                    calcc.CompileToCil(expr);
-                    calcc.AssembleToObjectCode();
-                    calcc.ExecuteObjectCode();
-                }, label);
+        //         // Act and Assert
+        //         var exception = Assert.ThrowsException<System.Reflection.TargetInvocationException>(() =>
+        //         {
+        //             var calcc = new CalcC();
+        //             calcc.CompileToCil(expr);
+        //             calcc.AssembleToObjectCode();
+        //             calcc.ExecuteObjectCode();
+        //         }, label);
 
-                // Assert
-                Assert.IsInstanceOfType(exception.InnerException, expectedExceptionType);
-            }
-        }
+        //         // Assert
+        //         Assert.IsInstanceOfType(exception.InnerException, expectedExceptionType);
+        //     }
+        // }
     }
 }
